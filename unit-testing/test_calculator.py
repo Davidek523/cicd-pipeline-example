dc1beb3ca327eee4e96d00c37ddb39fa8ee5254e
Davidek523 <davidov520@wp.pl>
2026-02-02 15:03:35 +0100

    CI/CD experiment
    added a ci file

--- unit-testing/test_calculator.py
+++ unit-testing/test_calculator.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 #-----------------------------------------------------------
 
-from calc.calculator import Calculator
+from src.calc.calculator import Calculator
 
 #-----------------------------------------------------------
 
